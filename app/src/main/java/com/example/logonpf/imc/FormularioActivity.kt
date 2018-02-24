c3fc5c6149cebbf0f3de3244ab0132eb38e504fb
logonpf <logonpf@fiap.com.br>
2018-02-23 23:03:26 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonpf/imc/FormularioActivity.kt
@@ -0,0 +1,35 @@
+package com.example.logonpf.imc
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_formulario.*
+
+class FormularioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario)
+
+        btLimpar.setOnClickListener {
+            inputAltura.editText?.setText("")
+            inputPeso.editText?.setText("")
+            inputPeso.requestFocus()
+        }
+
+        btCalcular.setOnClickListener {
+            val telaResultado = Intent(this,
+                    ResultadoActivity::class.java)
+
+            telaResultado.putExtra("PESO",
+                    inputPeso.editText?.text.toString())
+
+            telaResultado.putExtra("ALTURA",
+                    inputAltura.editText?.text.toString())
+
+            startActivity(telaResultado)
+        }
+
+
+    }
+}
